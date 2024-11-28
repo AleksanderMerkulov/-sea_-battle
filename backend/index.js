93d7aea36a21d81c3694588c7dc894f9ea29fbdb
Alex <agentov03@mail.ru>
2024-11-28 20:56:05 +1000

    прописал объект юзера

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,39 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+
+function create_user(socket_id, name){
+    return {
+        socket_id: socket_id,
+        name: name,
+        game_status: false, //готов к игре или нет
+        hod: false, //ходит или ждёт удара
+        game_card: {
+            4: [],
+            3: [],
+            2: [],
+            1: [],
+        }
+    }
+}
+
+users = {
+    0: create_user()
+
+}
+
+
 io.on('connection', (socket) => {
     console.log('New client connected');
 
+    socket.on('login', (user)=>{
 
+    })
+
+    socket.on('ready', ()=>{
+
+        socket.emit('rival_ready')
+    })
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
